4e58f3201f977577b1e4c4b626e22b201caf3f17
george <it01151@uom.edu.gr>
2017-01-28 19:19:08 +0200

    update

--- src/gairticketsjsonparsers/AirportParser.java
+++ src/gairticketsjsonparsers/AirportParser.java
@@ -5,6 +5,14 @@
  */
 package gairticketsjsonparsers;
 
+import com.sun.jndi.toolkit.url.Uri;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  *
  * @author george
@@ -29,6 +37,52 @@ public class AirportParser {
             return null;
         }
         
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String airportJsonStr = null;
+        
+        try {
+            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, params[0])
+                    .appendQueryParameter(FORMAT_PARAM, format)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                    .appendQueryParameter(APPID_PARAM, "27949ea6b6dffa1dad1deb925c9b024b")
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            airportJsonStr = buffer.toString();            
+            
+        } catch (IOException e) {
+            
+        }
         
     }
 }
